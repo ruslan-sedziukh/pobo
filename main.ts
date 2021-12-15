5022a74d50c1a2d2bed618a438c7532bdf3ce10b
Ruslan <ruslan.sedziukh@gmail.com>
2021-12-15 12:03:14 +0200

    Restore '13.12.2021, 17:59:55 0.9.2'

--- main.ts
+++ main.ts
@@ -470,11 +470,11 @@ forever(function () {
             story.spriteSayText(boss, "Твоє завдання - досягнути цієї цілі!")
             story.spriteSayText(Hops_and_Paw, "Якої цілі?")
             story.spriteSayText(boss, "Щоб ми стали найкращою компанією у світі! ")
-            story.spriteSayText(Hops_and_Paw, "Так, бос!")
+            story.spriteSayText(Hops_and_Paw, "Так, босе!")
             boss.setImage(assets.image`Intro Boss 2`)
-            story.spriteSayText(boss, "Вперед.")
-            story.spriteSayText(boss, "І не відволікайся на всяку фігню!")
+            story.spriteSayText(boss, "Вперед, щоб очі мої тебе не бачили.")
             controller.moveSprite(Hops_and_Paw, 100, 0)
+            story.spriteSayText(boss, "І не відволікайся на всяку фігню!")
             bossesDialog = 0
         }
     }
